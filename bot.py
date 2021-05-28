4900b332865c3de76b04563f0114f4fe74dabf7c
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-05-29 02:14:21 +0300

    Add def user_ask_full_moon

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import logging
+from math import e
 from os import sep
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 import ephem
-import datetime
+from datetime import timedelta, date, datetime
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -21,7 +22,7 @@ def user_ask_planet(update, context):
     text = update.message.text.split()
     user_planet = text[1].capitalize()
     if user_planet in planets:
-        planet = getattr(ephem, user_planet)(datetime.datetime.today().strftime("%Y/%m/%d"))
+        planet = getattr(ephem, user_planet)(date.today().strftime("%Y/%m/%d"))
         constellation = ephem.constellation(planet)
         update.message.reply_text(f'Сегодня эта планета находится в созведии: {constellation[1]}')
     else:
@@ -58,12 +59,31 @@ def user_ask_wordcount(update, context):
             update.message.reply_text(f'Кажется, что Вы не написали слов...')
 
 
+def user_ask_full_moon(update, context):
+    exceptions = ['.', '-', '_', ':']
+    text = update.message.text.split()[1]
+    for symbol in exceptions:
+        print(symbol)
+        if symbol in text:
+            text = text.split(sep=symbol)
+            updated_text = '/'.join(text)
+    try:
+        user_date = datetime.strptime(updated_text, '%Y/%m/%d')
+        moon = ephem.next_full_moon(user_date)
+        update.message.reply_text(f'Ближайшее полнолуние будет: {moon}')
+    except (ValueError, TypeError):
+        update.message.reply_text(f'{text} не является датой, введите дату в формате: гггг/мм/дд, гггг.мм.дд, гггг:мм:дд, гггг-мм-дд или гггг_мм_дд')
+
+        
+
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('planet', user_ask_planet))
     dp.add_handler(CommandHandler('wordcount', user_ask_wordcount))
+    dp.add_handler(CommandHandler('full_moon', user_ask_full_moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     logging.info('Бот стартовал')
